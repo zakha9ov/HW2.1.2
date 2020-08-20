3997773a2a98dddc91c93e34a7244cad801d65b9
Mikhail Zakharov <mikhailzakha9ov@gmail.com>
2020-08-20 14:19:32 +0300

    Finish hw2.1.2

--- HW2.1.2/ViewController.swift
+++ HW2.1.2/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var switchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redView.layer.cornerRadius = redView.frame.height / 2
+        redView.alpha = 0.3
+        
+        yellowView.layer.cornerRadius = yellowView.frame.height / 2
+        yellowView.alpha = 0.3
+        
+        greenView.layer.cornerRadius = greenView.frame.height / 2
+        greenView.alpha = 0.3
+        
+        switchButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func switchButtonPressed() {
+        if redView.alpha == 0.30000001192092896 && yellowView.alpha == 0.30000001192092896 && greenView.alpha == 0.30000001192092896 {
+            redView.alpha = 1
+            switchButton.setTitle("Next", for: .normal)
+        } else if redView.alpha == 1 && yellowView.alpha == 0.30000001192092896 && greenView.alpha == 0.30000001192092896 {
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+        } else if yellowView.alpha == 1 && redView.alpha == 0.30000001192092896 && greenView.alpha == 0.30000001192092896 {
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+        } else {
+            greenView.alpha = 0.3
+            redView.alpha = 1
+        }
+        
+        
+    }
+    
 }
+
+
 
